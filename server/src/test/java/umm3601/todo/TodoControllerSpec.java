e1933c4d7e35578fcf8f7a800b06c52add2a8a1c
Jackson Tenor <tenor031@umn.edu>
2019-02-28 14:14:49 -0600

    Test TodoController

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.*;
 public class TodoControllerSpec {
   private TodoController todoController;
   private ObjectId todoId;
+  private Map<String, String[]> queryParams;
 
   @Before
   public void clearAndPopulateDB() {
@@ -32,7 +33,7 @@ public class TodoControllerSpec {
     List<Document> testDocs = new ArrayList<>();
     testDocs.add(Document.parse("{\n" +
       "                    owner: \"KK\",\n" +
-      "                    status: \"true\",\n" +
+      "                    status: true,\n" +
       "                    body: \"Schedule donut walk\",\n" +
       "                    category: \"software design\"\n" +
       "                }"));
@@ -60,12 +61,97 @@ public class TodoControllerSpec {
     todoDocuments.insertOne(Document.parse(sam.toJson()));
 
     //Todo: write todoController code. Uncomment below when written.
-    //todoController = new TodoController(db);
+    todoController = new TodoController(db);
+    queryParams = new HashMap<>();
+
+  }
+
+  private static String getAttribute(BsonValue val, String key){
+    BsonDocument doc = val.asDocument();
+    return ((BsonString) doc.get(key)).getValue();
+  }
+  private static String getOwner(BsonValue val){
+    return getAttribute(val, "owner");
   }
 
   @Test
   public void getAllTodos() {
+    String jsonResult = todoController.getTodos(queryParams);
+    BsonArray docs = BsonArray.parse(jsonResult);
 
+    assertEquals("Should get 4 todos", 4, docs.size());
+    List<String> owners = docs.stream().map(TodoControllerSpec::getOwner).sorted().collect(Collectors.toList());
+    assertEquals("Owners should match", Arrays.asList("Jackson", "KK", "Nic", "Sam"), owners);
   }
+  @Test
+  public void getTodosByCategory() {
+    queryParams.put("category", new String[]{"software design"});
+    String jsonResult = todoController.getTodos(queryParams);
+    BsonArray docs = BsonArray.parse(jsonResult);
 
+    assertEquals("Should get 2 todos", 2, docs.size());
+    List<String> owners = docs.stream().map(TodoControllerSpec::getOwner).sorted().collect(Collectors.toList());
+    assertEquals("Owners should match", Arrays.asList("Jackson", "KK"), owners);
+  }
+  @Test
+  public void getTodosByOwner() {
+    queryParams.put("owner", new String[]{"Nic"});
+    String jsonResult = todoController.getTodos(queryParams);
+    BsonArray docs = BsonArray.parse(jsonResult);
+
+    assertEquals("Should get 1 todo", 1, docs.size());
+    List<String> owners = docs.stream().map(TodoControllerSpec::getOwner).sorted().collect(Collectors.toList());
+    assertEquals("Owners should match", Arrays.asList("Nic"), owners);
+  }
+  @Test
+  public void getTodosByStatus() {
+    queryParams.put("status", new String[]{"true"});
+    String jsonResult = todoController.getTodos(queryParams);
+    BsonArray docs = BsonArray.parse(jsonResult);
+    assertEquals("Should get 2 todos", 2, docs.size());
+
+    List<String> owners = docs.stream().map(TodoControllerSpec::getOwner).sorted().collect(Collectors.toList());
+    assertEquals("Owners should match", Arrays.asList("KK", "Sam"), owners);
+  }
+  @Test
+  public void getTodosContains() {
+    queryParams.put("contains", new String[]{"Lab"});
+    String jsonResult = todoController.getTodos(queryParams);
+    BsonArray docs = BsonArray.parse(jsonResult);
+    assertEquals("Should get 2 todos", 2, docs.size());
+
+    List<String> owners = docs.stream().map(TodoControllerSpec::getOwner).sorted().collect(Collectors.toList());
+    assertEquals("Owners should match", Arrays.asList("Jackson", "Nic"), owners);
+  }
+  @Test
+  public void getTodosMultipleParameters() {
+    queryParams.put("contains", new String[]{"Lab"});
+    queryParams.put("category", new String[]{"software design"});
+    String jsonResult = todoController.getTodos(queryParams);
+    BsonArray docs = BsonArray.parse(jsonResult);
+    assertEquals("Should get 1 todo", 1, docs.size());
+
+    List<String> owners = docs.stream().map(TodoControllerSpec::getOwner).sorted().collect(Collectors.toList());
+    assertEquals("Owners should match", Arrays.asList("Jackson"), owners);
+  }
+  @Test
+  public void getTodoById() {
+    String todoJson = todoController.getTodo(todoId.toHexString());
+    Document todo = Document.parse(todoJson);
+    assertEquals("Name should be Sam", "Sam", todo.get("owner"));
+    String todoJsonNotFound = todoController.getTodo(new ObjectId().toString());
+    assertNull("Todo should not exist", todoJsonNotFound);
+  }
+  @Test
+  public void addNewTodo() {
+    String newTodo = todoController.addNewTodo("Elena", "true", "data structures", "Write next lab");
+    assertNotNull("Todo Id should be returned when new todo is created", newTodo);
+    queryParams.put("owner", new String[]{"Elena"});
+    String jsonResult = todoController.getTodos(queryParams);
+    BsonArray docs = BsonArray.parse(jsonResult);
+
+    assertEquals("Should get 1 todo", 1, docs.size());
+    List<String> owners = docs.stream().map(TodoControllerSpec::getOwner).sorted().collect(Collectors.toList());
+    assertEquals("Owners should match", Arrays.asList("Elena"), owners);
+  }
 }
