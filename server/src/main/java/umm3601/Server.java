eb965c77e9808e4595cd50c969fda5ede261fc7a
nkfoss <nathankfoss@gmail.com>
2019-02-27 19:47:40 -0600

    Expand server functionality for Todos

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -12,15 +12,20 @@ import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Server {
   private static final String userDatabaseName = "dev";
+  private static final String todoDatabaseName = "dev"; // ?maybe not necessary?
   private static final int serverPort = 4567;
 
   public static void main(String[] args) {
 
     MongoClient mongoClient = new MongoClient();
     MongoDatabase userDatabase = mongoClient.getDatabase(userDatabaseName);
+    MongoDatabase todoDatabase = mongoClient.getDatabase(todoDatabaseName);
 
     UserController userController = new UserController(userDatabase);
     UserRequestHandler userRequestHandler = new UserRequestHandler(userController);
+
+    TodoController todoController = new todoController(todoDatabase);
+    TodoRequestHandler todoRequestHandler = new TodoRequestHandler(todoController);
 
     //Configure Spark
     port(serverPort);
@@ -63,6 +68,10 @@ public class Server {
     get("api/users", userRequestHandler::getUsers);
     get("api/users/:id", userRequestHandler::getUserJSON);
     post("api/users/new", userRequestHandler::addNewUser);
+
+    get("api/todos", todoRequestHandler::getTodos);
+    get("api/todos/:id", todoRequestHandler::getTodoJSON);
+    post("api/todos/new", todoRequestHandler::addNewTodo);
 
     // An example of throwing an unhandled exception so you can see how the
     // Java Spark debugger displays errors like this.
